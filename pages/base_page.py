cf10cd4399855274317ff18708c432ffe120aeb2
Ivan Grebenschikov <albonigrum@gmail.com>
2020-08-27 20:44:59 +0300

    Commit for https://stepik.org/lesson/238819/step/6?unit=211271, add BasePage.is_element_present(), rewrite MainPage.should_be_login_link()

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,20 @@
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    def __init__(self, browser: RemoteWebDriver, url: str):
+    def __init__(self, browser: RemoteWebDriver, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        self.timeout = timeout
+        self.browser.implicitly_wait(self.timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
