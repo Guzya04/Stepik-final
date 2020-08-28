e7c1c4a7a82e509fd97f21174285ad49ccf1191f
Ivan Grebenschikov <albonigrum@gmail.com>
2020-08-28 21:07:50 +0300

    Commit for https://stepik.org/lesson/201964/step/14?unit=176022, final version of project.

--- test_product_page.py
+++ test_product_page.py
@@ -24,12 +24,22 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
         page.open()
         page.click_button_add_to_basket()
         page.should_check_adding_product_to_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_button_add_to_basket()
+    page.should_check_adding_product_to_basket()
 
 
 @pytest.mark.xfail
@@ -51,24 +61,26 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
-    basket_page = BasketPage(page.browser, page.browser.current_url)
+    basket_page = BasketPage(browser, browser.current_url)
     basket_page.basket_should_contains_n_different_position()
     basket_page.should_be_text_about_empty_basket()
